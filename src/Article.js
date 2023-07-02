68cd349e43c0807a1a78a1e65b05845c96e2c7f2
BLKTECHNERD <honeyhw@yahoo.com>
2023-07-02 19:19:42 -0400

    initial commit

---
+++ src/Article.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Article() {
+  return (
+    <div>
+      <article>
+        <p className="article-date">11/12/20</p>
+        <h2>On the Street in Brooklyn</h2>
+        <img src={require('./images/blog-image-1.jpg')} alt="Article 1" />
+        <p className="drop-cap">Cray ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a href="#" className="continue-link">Continues...</a>
+      </article>
+
+      <article>
+        <p className="article-date">11/11/20</p>
+        <h2>Vintage in Vogue</h2>
+        <img src={require('./images/blog-image-2.jpg')} alt="Article 2" />
+        <p className="drop-cap">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a href="#" className="continue-link">Continues...</a>
+      </article>
+    </div>
+  );
+}
+
+export default Article;
